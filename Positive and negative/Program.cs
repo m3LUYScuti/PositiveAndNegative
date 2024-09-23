27c24c1fe50fb2bda7114c4b9cf5fd7f160fb52c
davit <davit@GOSH>
2024-09-23 17:31:46 +0400

    Add project files.

---
+++ Positive and negative/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Airchiet Mteli Cifri");
+
+int result = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("Choos Second number");
+//double number2 = double.Parse(Console.ReadLine());
+
+//Console.WriteLine("Choos symbol from: (+, -, *, /, %,) ");
+//String symbol = Console.ReadLine();
+
+
+if (result < 0)
+{
+    Console.WriteLine("Cifri uaryofitia");
+}
+else if (result > 0)
+{
+    Console.WriteLine("cifri dadebitia");
+}
+else if (result == 0)  
+{
+    Console.WriteLine("Cifri nolis tolia");
+}
+
+
+Console.ReadLine();
